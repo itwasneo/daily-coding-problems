57340ecd3977b8ca87c59ab5252eddad5e6d9e35
itwasneo <kaan.yildiz@protonmail.com>
2023-03-27 18:58:16 +0300

    Initial commit

---
+++ rust/src/solutions/problem_132.rs
@@ -0,0 +1,14 @@
+#[allow(dead_code)]
+pub fn solve() -> i32 {
+    132
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solve;
+
+    #[test]
+    fn is_working() {
+        assert_eq!(132, solve());
+    }
+}
